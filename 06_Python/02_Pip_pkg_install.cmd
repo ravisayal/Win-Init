272b9bd343116a8340f7b7f922f378dd9188583c
ravisayal <ravisayal@yahoo.com>
2021-06-12 11:28:44 -0400

    Update 02_Pip_pkg_install.cmd

--- 06_Python/02_Pip_pkg_install.cmd
+++ 06_Python/02_Pip_pkg_install.cmd
@@ -3,3 +3,9 @@ REM  Pip packages
 pip install jupyter-lab
 pip install numpy
 pip install cx_Oracle
+
+## OpenCV
+# Option 1 - Main modules package: pip install opencv-python
+pip install opencv-python  
+# Option 2 - Full package (contains both main modules and contrib/extra modules):  
+pip install opencv-contrib-python 
